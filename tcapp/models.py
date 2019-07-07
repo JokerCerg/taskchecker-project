a479b28f29908ffe51aa6f77149f55f749a897c1
JokerCerg <3112joker3112@gmail.com>
2019-07-07 18:39:19 +0300

    create Task model create view Task on landing

--- tcapp/models.py
+++ tcapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    body = models.TextField(blank=True, db_index=True)
+
+    def __str__(self):
+        return self.title
+
--- tcapp/views.py
+++ tcapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Task
+
 
 def tasks_list(request):
-    return render(request, 'tcapp/task_list.html')
+    tasks = Task.objects.all()
+    return render(request, 'tcapp/task_list.html', context={"tasks": tasks})
