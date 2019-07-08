3b03c9ac2fd958d9d558f0c29cfed07549714be2
Сергей Юров <3112Joker3112@gmail.com>
2019-07-08 03:35:52 +0300

    add configuration

--- tcproject/settings.py
+++ tcproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8ov9s6ijgm7vk(=xe$9^efgfjdxj5w*#rnf&&b%ts549h-$urw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
 
 
 # Application definition
