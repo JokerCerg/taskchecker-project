4cb78de558fe4aa7f743591e0bfe1d12eb5b0c8d
JokerCerg <3112joker3112@gmail.com>
2019-07-14 22:15:26 +0300

    create mixin for create Task model

--- tcapp/views.py
+++ tcapp/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from .utils import ObjectDetailMixin
-
+from .utils import *
 
 from .models import Task
 from .forms import TaskForm
 
 
 def tasks_list(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().filter()
     return render(request, 'tcapp/task_list.html', context={'tasks': tasks})
 
 
@@ -17,16 +16,8 @@ class TaskDetail(ObjectDetailMixin, View):
     template = 'tcapp/task_detail.html'
 
 
-class TaskCreate(View):
-    def get(self, request):
-        form = TaskForm()
-        return render(request, 'tcapp/task_create.html', context={'form': form})
+class TaskCreate(ObjectCreateMixin, View):
+    model_form = TaskForm
+    template = 'tcapp/task_create.html'
 
-    def post(self, request):
-        bound_form = TaskForm(request.POST)
-
-        if bound_form.is_valid():
-            new_task = bound_form.save()
-            return redirect(new_task)
-        return render(request, 'tcapp/task_create.html', context={'form': bound_form})
 
